b2bead60445c9a9c76a08bb5bbe05ba97c38ea47
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-05-17 19:53:35 -0400

    created inputChanges function, added value attributes to the form elements

--- src/components/Form.js
+++ src/components/Form.js
@@ -15,16 +15,30 @@ function Form() {
         instructions: null
     });
 
+    //function to set the state when the input changes
+    const inputChanges = (e) => {
+        let value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+        setFormState({ ...formState, [e.target.name]: value });
+        console.log(formState);
+    };
+
     //the actual form goes in the return statement
     return (
         <form>
             <label htmlFor='name'>
                 Name:
-                <input type='text' name='name' id='name' placeholder='Enter your name...'/>
+                <input 
+                    type='text' 
+                    name='name' 
+                    id='name' 
+                    placeholder='Enter your name...'
+                    value={formState.name}
+                    onChange={inputChanges}
+                />
             </label>
             <label htmlFor='size'>
                 Pizza Size:
-                <select name='size' id='size'>
+                <select value={formState.size} name='size' id='size'>
                     <options value=''>Select a size...</options>
                     <options value='small'>Small</options>
                     <options value='medium'>Medium</options>
@@ -34,22 +48,51 @@ function Form() {
             </label>
             <label htmlFor='cheese'>
                 Mozzarella Cheese
-                <input type='checkbox' name='cheese' id='cheese'/>
+                <input 
+                    type='checkbox' 
+                    name='cheese' 
+                    id='cheese'
+                    checked={formState.cheese}
+                    onChange={inputChanges}
+                />
             </label>
             <label htmlFor='ham'>
                 Ham
-                <input type='checkbox' name='ham' id='ham'/>
+                <input 
+                    type='checkbox' 
+                    name='ham' 
+                    id='ham'
+                    checked={formState.ham}
+                    onChange={inputChanges}
+                />
             </label>
             <label htmlFor='olives'>
                 Black Olives
-                <input type='checkbox' name='olives' id='olives'/>
+                <input 
+                    type='checkbox' 
+                    name='olives' 
+                    id='olives'
+                    checked={formState.olives}
+                    onChange={inputChanges}
+                />
             </label>
             <label htmlFor='pineapple'>
                 Pineapple
-                <input type='checkbox' name='pineapple' id='pineapple'/>
+                <input 
+                    type='checkbox' 
+                    name='pineapple' 
+                    id='pineapple'
+                    checked={formState.pineapple}
+                    onChange={inputChanges}
+                />
             </label>
             <label htmlFor='instructions'>
-                <textarea id='instructions' name='instructions'/>
+                <textarea 
+                    id='instructions' 
+                    name='instructions'
+                    value={formState.instructions}
+                    onChange={inputChanges}
+                />
             </label>
         </form>
     );
